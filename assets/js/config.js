39ae7e0a62471a43457d4e862aab672e89e05a4f
Kittunaik <159645431+Mrkittunaik@users.noreply.github.com>
2026-06-16 09:41:51 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,4 +3,4 @@
 // Set this to your deployed backend URL (no trailing slash).
 // Example: "https://dmflow-backend.railway.app"
 // Example: "https://api.dmflow.in"
-window.DMFLOW_API_URL = "https://dmflow-backend-u9h8.onrender.com"; // 👈 PUT YOUR BACKEND URL HERE
+window.DMFLOW_API_URL = "https://api.dmflowapp.in"; // 👈 PUT YOUR BACKEND URL HERE
